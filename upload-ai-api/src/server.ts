1fa333d5bb50a4e474ee90bccc1764dfbd6d12c4
humbertoromanojr <astronomi@gmail.com>
2023-09-14 14:37:01 -0300

    Update server

--- upload-ai-api/src/server.ts
+++ upload-ai-api/src/server.ts
@@ -6,6 +6,10 @@ app.get("/", () => {
   return "Hello world!!!"
 })
 
-app.listen({
-  port: 3333,
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("== HTTP Server Running > => ")
+  })
