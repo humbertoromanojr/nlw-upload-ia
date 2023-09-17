69294eb2b0a1ca3f02ec05ff5b05ba88aa0452c2
humbertoromanojr <astronomi@gmail.com>
2023-09-17 09:49:00 -0300

    Setting generate-ai-completion.ts route

---
+++ upload-ai-api/src/routes/generate-ai-completion.ts
@@ -0,0 +1,16 @@
+import { FastifyInstance } from "fastify"
+import { z } from "zod"
+
+export async function generateAICompletionRoute(app: FastifyInstance) {
+  app.post("/ai/complete", async (req) => {
+    const bodySchema = z.object({
+      videoId: z.string().uuid(),
+      template: z.string(),
+      temperature: z.number().min(0).max(1).default(0.5),
+    })
+
+    const { videoId, template, temperature } = bodySchema.parse(req.body)
+
+    return { videoId, template, temperature }
+  })
+}
