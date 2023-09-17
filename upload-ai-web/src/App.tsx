cac49c85e5c86ab107524e0fc52334c7aff5ffbe
humbertoromanojr <astronomi@gmail.com>
2023-09-17 10:29:46 -0300

    Setting VideoInputForm component

--- upload-ai-web/src/App.tsx
+++ upload-ai-web/src/App.tsx
@@ -7,9 +7,11 @@ import {
   SelectTrigger,
   SelectValue,
 } from "./components/ui/select"
+
 import { Separator } from "./components/ui/separator"
 import { Slider } from "./components/ui/slider"
 import { Textarea } from "./components/ui/textarea"
+import { VideoInputForm } from "./components/video-input-form"
 
 export function App() {
   return (
@@ -51,39 +53,7 @@ export function App() {
         </div>
 
         <aside className="w-88 space-y-6">
-          <form className="space-y-6">
-            <label
-              htmlFor="video"
-              className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
-            >
-              Selecione um video
-            </label>
-
-            <input
-              type="file"
-              id="video"
-              accept="video/mp4"
-              className="sr-only"
-            />
-
-            <Separator />
-
-            <div className="space-y-2">
-              <Label htmlFor="transcription_prompt">
-                Prompt de transcrição
-              </Label>
-
-              <Textarea
-                id="transcription_prompt"
-                className="h-20 leading-relaxed resize-none"
-                placeholder="Inclua palavras-chave mencionadas no video separadas por vírgula (,)"
-              />
-            </div>
-
-            <Button type="submit" className="w-full">
-              Carregar video
-            </Button>
-          </form>
+          <VideoInputForm />
 
           <Separator />
 
