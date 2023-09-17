c85f0d791e313d41683ee1ac80f9c0baee4fe924
humbertoromanojr <astronomi@gmail.com>
2023-09-17 10:55:41 -0300

    Update Event function handleFileSelected

--- upload-ai-web/src/components/video-input-form.tsx
+++ upload-ai-web/src/components/video-input-form.tsx
@@ -1,19 +1,40 @@
+import { useState } from "react"
 import { Button } from "./ui/button"
 import { Label } from "./ui/label"
 import { Separator } from "./ui/separator"
 import { Textarea } from "./ui/textarea"
 
 export function VideoInputForm() {
+  const [videoFile, setVideoFile] = useState<File | null>(null)
+
+  function handleFileSelected(event: ChangeEvent<HTMLInputElement>) {
+    const { files } = event.currentTarget
+
+    if (!files) {
+      return
+    }
+
+    const selectedFile = files[0]
+
+    setVideoFile(selectedFile)
+  }
+
   return (
     <form className="space-y-6">
       <label
         htmlFor="video"
         className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
       >
-        Selecione um video
+        {videoFile ? "olá video" : "Selecione um video"}
       </label>
 
-      <input type="file" id="video" accept="video/mp4" className="sr-only" />
+      <input
+        type="file"
+        id="video"
+        accept="video/mp4/mov"
+        className="sr-only"
+        onChange={handleFileSelected}
+      />
 
       <Separator />
 
